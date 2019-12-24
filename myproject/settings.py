3b85d67f1aa4e5b2659256873785a104ed2c3613
isaacdias <isaacrdias@gmail.com>
2019-12-24 11:42:05 -0300

    alteração do endereço

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['blog-olinda-heroku.com', 'localhost:8000']
+ALLOWED_HOSTS = ['blog-olinda-herokuapp.com', 'localhost:8000']
 
 # Application definition
 
