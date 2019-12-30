a1015ba51edfe6dace49bbc0df6301a382ab54b8
isaacdias <isaacrdias@gmail.com>
2019-12-30 07:57:15 -0300

    retirando o Cling

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -7,13 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-from dj_static import Cling
-    
+import os    
 
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
