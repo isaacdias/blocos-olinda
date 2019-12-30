333a764f31eb7654478e3230dddca10c359f7fdd
isaacdias <isaacrdias@gmail.com>
2019-12-30 10:28:07 -0300

    registrando o model post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at', 'status')
+    search_fields = ['title']
+    prepopulated_fields = {'slug':('title',)}
+
+
+admin.site.register(Post, PostAdmin)
