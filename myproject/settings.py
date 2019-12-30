c496e4a08e213bccc9e7cf71a4252bec7cc95d40
isaacdias <isaacrdias@gmail.com>
2019-12-30 11:34:26 -0300

    erro SECRET_KEY

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'config('SECRET_KEY')'
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['blog-olinda.herokuapp.com', 'localhost:8000']
